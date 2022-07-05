6df40f94effcc486c310fe2489e6259c3987cd1a
diyglobe <108207883+diyglobe@users.noreply.github.com>
2022-07-05 18:10:49 +0800

    Update update.sh

--- update/update.sh
+++ update/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Script Auto Reboot Vps
 #wget https://github.com/${GitUser}/
-GitUser="coding006"
+GitUser="diyglobe"
 #IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
 # Valid Script
